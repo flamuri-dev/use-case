5f75286bb9a4fe8c6f158da3fb4ca5c5e5873f17
flamuri-dev <108237346+flamuri-dev@users.noreply.github.com>
2022-07-05 18:40:54 +0100

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -78,7 +78,7 @@ function Info() {
 
                 connectedContract.on("NewNFTMinted", (from, tokenId) => {
                     console.log(from, tokenId.toNumber())
-                    alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+                    console.log("Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/"+CONTRACT_ADDRESS+"/"+tokenId.toNumber())
                 });
 
                 console.log("Setup event listener!")
