575fc209ad4fb9174b6e2f332bd77eea798dc73a
flamuri-dev <108237346+flamuri-dev@users.noreply.github.com>
2022-07-05 19:00:44 +0100

    Update Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ function Interests() {
     return (
         <div className="interestsContainer">
             <h2 className="interests--title">Thank you...</h2>
-            <p className="interests--text">for choosing us for your recent stay in Porto. We look forward to welcoming you!</p>
+            <p className="interests--text">...for choosing us for your recent stay in Porto. We look forward to welcoming you!</p>
         </div>
     )
 }
