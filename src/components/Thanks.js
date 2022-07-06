b7975260c0e3d98d4b56ea22c1527f9f7d53ce3b
flamuri-dev <108237346+flamuri-dev@users.noreply.github.com>
2022-07-06 14:41:26 +0100

    Update Thanks.js

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -3,7 +3,7 @@ import React from "react"
 function Thanks() {
     return (
         <div className="thanksContainer">
-            <h2 className="thanks--title">Thank you...</h2>
+            <h2 className="thanks--title">Thank you</h2>
             <p className="thanks--text">...for choosing us for your recent stay in Porto. We look forward to welcoming you!</p>
         </div>
     )
